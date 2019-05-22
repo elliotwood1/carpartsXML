460f8e4a9dc8e445164f9b7fee3f4c0023172657
elliotwood1 <47351132+elliotwood1@users.noreply.github.com>
2019-05-22 20:15:04 +0100

    /

--- testXML.js
+++ testXML.js
@@ -1,18 +1,38 @@
-$(document).ready(function () {
-    $.ajax({
-        type: "GET",
-        url: "carpartsfeedawin.xml",
-        dataType: "xml",
-        success: parseXml
-    });
+$(document).ready(function() {
+  $.ajax({
+    type: "GET",
+    url: "carpartsfeedawin.xml",
+    // url: "http://www.affiliatewindow.com/DTD/affiliate/datafeed.1.5.dtd",
+    dataType: "xml",
+    success: parseXml
+  });
 });
 
 function parseXml(xml) {
-    $("#container").html("<ul id='content' data-role='listview' data-inset='true'></ul>");
-    $(xml).find("prod").each(function () {
-        $("#container").append("<li><a href='" + $(this).find("mImage").text() + "'><img src='" + $(
-                this)
-            .find("mImage").text() + "'/><h2>" + $(this).find("mCat").text() + "</h2><p>" + $(this)
-            .find("buynow").text() + "</p></a></li>");
+  $("#container").html(
+    "<ul id='content' data-role='listview' data-inset='true'></ul>"
+  );
+  $(xml)
+    .find("prod")
+    .each(function() {
+      $("#container").append(
+        "<li><a href='" +
+          $(this)
+            .find("mImage")
+            .text() +
+          "'><img src='" +
+          $(this)
+            .find("mImage")
+            .text() +
+          "'/><h2>" +
+          $(this)
+            .find("mCat")
+            .text() +
+          "</h2><p>" +
+          $(this)
+            .find("buynow")
+            .text() +
+          "</p></a></li>"
+      );
     });
 }
